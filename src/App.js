75a5720a6413e3ad9feda21cc7da4dcdf0d4bfc4
Nick Saravia <nick.saravia@gmail.com>
2021-03-22 21:44:58 -0400

    prettier

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button } from '@material-ui/core';
-import createArray from './methods';
-import { useState, useEffect } from 'react';
-import Line from './components/line';
-import styled from 'styled-components';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@material-ui/core";
+import createArray from "./methods";
+import { useState, useEffect } from "react";
+import Line from "./components/line";
+import styled from "styled-components";
 
 function App() {
-
-  const [array, setArray] = useState([])
+  const [array, setArray] = useState([]);
   useEffect(() => {
     const arr = createRandomArray();
     setArray(arr);
   }, []);
 
   const bubbleSort = () => {
-    console.log("hi");
-      let swap = false;
-      let arrayCopy = [...array];
-      
-      do {
-        console.log("hey there")
-        swap = false;
-        for (let i = 0; i < arrayCopy.length; i++) {
-          console.log(i + 1);
-          if (i + 1 <= arrayCopy.length - 1 && arrayCopy[i].height > arrayCopy[i + 1].height) {
-            const temp = arrayCopy[i];
-            arrayCopy[i].selected = true;
-            arrayCopy[i] = arrayCopy[i + 1];
-            arrayCopy[i + 1] = temp;
-            swap = true;
-          }else{
-          }
-        }
-      } while (swap);
-      console.log(arrayCopy);    
-  }
+    let swap = false;
+    let arrayCopy = [...array];
 
+    do {
+      swap = false;
+      for (let i = 0; i < arrayCopy.length; i++) {
+        if (
+          i + 1 <= arrayCopy.length - 1 &&
+          arrayCopy[i].height > arrayCopy[i + 1].height
+        ) {
+          const temp = arrayCopy[i];
+          arrayCopy[i].selected = true;
+          arrayCopy[i] = arrayCopy[i + 1];
+          arrayCopy[i + 1] = temp;
+          swap = true;
+        } else {
+        }
+      }
+    } while (swap);
+  };
 
   const createRandomArray = () => {
     const randomNum = Math.floor(Math.random() * 100 + 1);
@@ -45,7 +42,7 @@ function App() {
   };
 
   const StyledDiv = styled.div`
-  padding: 20px;
+    padding: 20px;
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(2px, 1fr));
     gap: 10px;
