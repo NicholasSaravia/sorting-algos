dd31328cf7861c1e40b188c627c1655d33841d6e
Nick Saravia <nick.saravia@gmail.com>
2021-03-29 23:00:04 -0400

    somewhat working

--- src/components/bubble/selection.js
+++ src/components/bubble/selection.js
@@ -2,7 +2,11 @@ import { Button, SwipeableDrawer } from "@material-ui/core";
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
-import { setCurrentIndex, updateArrayPositions } from "../../store/arraySlice";
+import {
+  setComparedIndex,
+  setCurrentIndex,
+  updateArrayPositions,
+} from "../../store/arraySlice";
 
 export const Selection = () => {
   let array = useSelector((state) => state.array.value);
@@ -19,12 +23,23 @@ export const Selection = () => {
     for (let i = 0; i < arrayCopy.length; i++) {
       let smallestNumIndex = i;
       for (let j = 1; j < arrayCopy.length; j++) {
-        if (arrayCopy[i] > arrayCopy[j]) {
+        
+        let promisOne = new Promise((resolve, reject) => {
+          setTimeout(() => {
+            dispatch(setComparedIndex(j));
+            resolve();
+          }, 100);
+        });
+
+        if (arrayCopy[i].height > arrayCopy[j].height) {
           let promise = new Promise((resolve, reject) => {
-            dispatch(updateArrayPositions({ index1: i, index2: j }));
-            dispatch(setCurrentIndex(j));
+            setTimeout(() => {
+              dispatch(updateArrayPositions({ index1: i, index2: j }));
+              dispatch(setCurrentIndex(j));
+              resolve();
+            }, 100);
           });
-          await promise;
+          await Promise.all([promisOne, promise]);
 
           swap(i, j, arrayCopy);
           smallestNumIndex = j;
