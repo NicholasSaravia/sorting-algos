70334027a5ec25aa1d42559a0b8af33cf32de8ba
Nick Saravia <nick.saravia@gmail.com>
2021-03-31 09:11:02 -0400

    fixed selection sort

--- src/components/bubble/selection.js
+++ src/components/bubble/selection.js
@@ -12,21 +12,26 @@ export const Selection = () => {
   let array = useSelector((state) => state.array.value);
   const dispatch = useDispatch();
 
-  const swap = (i, j, arrayCopy) => {
-    console.log({ arrayCopy, i, j });
-    const temp = arrayCopy[j];
-    arrayCopy[j] = arrayCopy[i];
-    arrayCopy[i] = temp;
-  };
+  const swap = (i, j, arrayCopy) => {};
 
   const selectionSort = async () => {
     let arrayCopy = [...array];
-    let smallestIndex = 0;
     let comparedIndex = 1;
+
     // loop through array once.
     for (let i = 0; i < arrayCopy.length; i++) {
+      let smallestIndex = i;
+
+      let setMallestIndexPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          dispatch(setCurrentIndex(smallestIndex));
+          resolve();
+        }, 100);
+      });
+      await Promise.all([setMallestIndexPromise]);
+
       // itterate through array swapping indexes if right side is bigger than left
-      for (let j = 1; j < arrayCopy.length; j++) {
+      for (let j = i + 1; j < arrayCopy.length; j++) {
         // set compared index - orange
         let promiseOne = new Promise((resolve, reject) => {
           setTimeout(() => {
@@ -37,32 +42,33 @@ export const Selection = () => {
         });
         await Promise.all([promiseOne]);
 
-        // if right is smaller than left, then swap places
-        if (arrayCopy[smallestIndex].height > arrayCopy[comparedIndex].height) {
-          // swap places and set current index (pink) to current index
-          let promise = new Promise((resolve, reject) => {
-            setTimeout(() => {
-              // update local array
-              swap(smallestIndex, comparedIndex, arrayCopy);             
-              dispatch(updateArrayPositions({ index1: smallestIndex, index2: comparedIndex }));
+        // if right is smaller than left, then update smallest index to be j
+        // we do not swap right away because there could be a smaller index;
+        if (arrayCopy[smallestIndex].height > arrayCopy[j].height) {
+          smallestIndex = j;
+        }
+      }
 
-              smallestIndex = j;
-              comparedIndex = j + 1;
-              // update state array
-              dispatch(setCurrentIndex(smallestIndex));
-              dispatch(setComparedIndex(comparedIndex));
-
-              resolve();
-            }, 100);
-          });
-          await Promise.all([promise]);
-        } 
+      if (smallestIndex != i) {
+        let swapPromise = new Promise((resolve, reject) => {
+          setTimeout(() => {
+            console.log({ arrayCopy, i, smallestIndex});
+            const temp = arrayCopy[smallestIndex];
+            arrayCopy[smallestIndex] = arrayCopy[i];
+            arrayCopy[i] = temp;
+            dispatch(
+              updateArrayPositions({ index1: i, index2: smallestIndex })
+            );
+            resolve();
+          }, 100);
+        });
+        await Promise.all([swapPromise]);
       }
     }
   };
 
   return (
-    <Button onClick={() => selectionSort()} variant="contained" color="default">
+    <Button onClick={() => selectionSort()} variant="contained" color="secondary">
       Selection Sort
     </Button>
   );
